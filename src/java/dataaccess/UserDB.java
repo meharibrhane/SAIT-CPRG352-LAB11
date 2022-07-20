35b2989ee9808a0805c46e341da12539ff1816d4
meharibrhane <meharifessehaye@gmail.com>
2022-07-19 22:28:30 -0600

    Email Java Web Application WEB-INF and Source packages modified

---
+++ src/java/dataaccess/UserDB.java
@@ -0,0 +1,32 @@
+package dataaccess;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import models.User;
+
+
+public class UserDB {
+    public User get(String email) {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        
+        try {
+            User user = em.find(User.class, email);
+            return user;
+        } finally {
+            em.close();
+        }
+    }
+
+    public User getByUUID(String uuid) {
+         EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        
+        try {
+            Query query = em.createNamedQuery("User.findByResetPasswordUuid");
+            query.setParameter("resetPasswordUuid", uuid); 
+            User uuidResult = (User)query.getSingleResult();  
+            return uuidResult;
+        } finally {
+            em.close();
+        }
+    }
+}
---
+++ src/java/servlets/ResetPasswordServlet.java
@@ -0,0 +1,68 @@
+
+package servlets;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import services.AccountService;
+
+/**
+ *
+ * @author mehar
+ */
+public class ResetPasswordServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        String uuid = request.getParameter("uuid");
+        
+        if (uuid != null) {
+            getServletContext().getRequestDispatcher("/WEB-INF/resetNewPassword.jsp").forward(request, response);
+            return; 
+        }
+        
+        
+        getServletContext().getRequestDispatcher("/WEB-INF/reset.jsp").forward(request, response);
+        return; 
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       
+         String url = request.getRequestURL().toString(); 
+        AccountService accountService = new AccountService();
+       
+        String action = request.getParameter("action");
+        String email  = request.getParameter("email");
+        String path = getServletContext().getRealPath("/WEB-INF");
+       
+        if (action != null) {
+            if (action.equals("reset")) {
+                try {    
+                    accountService.resetPassword(email, path, url);
+                } catch (Exception ex) {
+                    Logger.getLogger(ResetPasswordServlet.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+            
+            if (action.equals("newpassword")) {
+                String uuid = request.getParameter("uuid");
+                String password = request.getParameter("password");
+                accountService.changePassword(uuid, password);
+                
+                getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+                return; 
+            }
+        }
+        getServletContext().getRequestDispatcher("/WEB-INF/reset.jsp").forward(request, response);
+        return; 
+    }
+
+}
